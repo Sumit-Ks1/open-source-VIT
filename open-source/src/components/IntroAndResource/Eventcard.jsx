96766b5f65dce049dc907566818922eb39bc2fd5
Sumit-Ks1 <sumit127624@gmail.com>
2024-06-15 18:01:36 +0530

    Added some css to intro.module.css

---
+++ open-source/src/components/IntroAndResource/Eventcard.jsx
@@ -0,0 +1,13 @@
+
+export default function Eventcard(props) {
+    return (
+        <div >
+            <h3 >
+                {props.currentStatus}
+            </h3>
+            <div >
+                {props.image}
+            </div>
+        </div >
+    )
+}
--- open-source/src/components/IntroAndResource/Reso.jsx
+++ open-source/src/components/IntroAndResource/Reso.jsx
@@ -1,6 +1,26 @@
+import reso from './reso.module.css';
+import Eventcard from './Eventcard.jsx';
+
 
 export default function Reso() {
+
+
+
   return (
-    <div>Reso</div>
+    <div classsName={reso.eventcontainer}>
+      <h1 className={reso.eventheading}>
+        Events
+      </h1>
+      <div className={reso.eventcardcontainer}>
+        <Eventcard currentStatus="UPCOMING" eventName="Evolove with AI"  field="Artificial Intelligence" description="“Evolve with AI” is an immersive event showcasing the latest AI innovations, offering hands-on experiences, expert talks, and networking opportunities to shape the future of technology."/>
+        {/* <Eventcard currentStatus="" eventName="" image={} field="" description=""/>
+        <Eventcard currentStatus="" eventName="" image={} field="" description=""/>
+        <Eventcard currentStatus="" eventName="" image={} field="" description=""/>
+        <Eventcard currentStatus="" eventName="" image={} field="" description=""/>
+        <Eventcard currentStatus="" eventName="" image={} field="" description=""/>
+        <Eventcard currentStatus="" eventName="" image={} field="" description=""/>
+        <Eventcard currentStatus="" eventName="" image={} field="" description=""/> */}
+      </div>
+    </div>
   )
 }
