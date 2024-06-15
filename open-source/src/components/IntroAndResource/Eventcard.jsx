7904a2d7f4f62f207c60cc5e0d5fc2bcdba2666f
Sumit-Ks1 <sumit127624@gmail.com>
2024-06-15 22:44:50 +0530

    Added a new div in eventcard and css in it

--- open-source/src/components/IntroAndResource/Eventcard.jsx
+++ open-source/src/components/IntroAndResource/Eventcard.jsx
@@ -3,19 +3,21 @@ export default function Eventcard(props) {
     return (
         <div className={card.cardcontainer}>
             <h3 className={card.cardstatus}>
-                {props.currentStatus}
+                {props.currentStatus} ....
             </h3>
-            <h2 className={card.cardeventname}>
-                {props.eventName}
-            </h2>
-            <div className={card.cardimage}>
-                {props.image}
-            </div>
-            <h3 className={card.cardfield}>
-                {props.field}
-            </h3>
-            <div className={card.carddescription}>
-                {props.description}
+            <div className={card.innerconatiner}>
+                <h2 className={card.cardeventname}>
+                    {props.eventName}
+                </h2>
+                <div className={card.cardimage}>
+                    {props.image}
+                </div>
+                <h3 className={card.cardfield}>
+                    {props.field}
+                </h3>
+                <div className={card.carddescription}>
+                    {props.description}
+                </div>
             </div>
         </div >
     )
